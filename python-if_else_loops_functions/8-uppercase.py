b86897d11f9a8a5dc975744c7a86907603036947
Katrolli <ennio_cano@live.com>
2023-01-16 10:30:02 -0800

    to uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    new_str = ""
+    for char_new in str:
+        if ord(char_new) >= 97 and ord(char_new) <= 122:
+            new_str += chr(ord(char_new) - 32)
+        else:
+            new_str += char_new
+    print(new_str)
