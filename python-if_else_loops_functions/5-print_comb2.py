4ffa557d73eb1007bf4935a4b559feee33b417d6
Katrolli <ennio_cano@live.com>
2023-01-16 09:20:24 -0800

    from 00 to 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("0{}, ".format(i), end="")
-    elif i <= 98:
-        print("{}, ".format(i), end="")
+    if i <= 98:
+        print("{:02d}".format(i), end=", ")
     else:
         print(i)
