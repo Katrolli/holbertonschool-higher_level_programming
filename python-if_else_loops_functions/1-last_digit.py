8f005cba122cf59c9cd88c26573fd4f5cc5b3994
Katrolli <ennio_cano@live.com>
2023-01-16 07:05:37 -0800

    last digit of positive and negative nr

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,18 +3,18 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    l = (number * -1 % 10) * -1
-    if l > 5:
-        print(f"Last digit of {number} is {l} and is greater than 5")
-    elif l == 0:
-        print(f"Last digit of {number} is {l} and is 0")
-    elif l < 6 and l != 0:
-        print(f"Last digit of {number} is {l} and is less than 6 and not 0")
+    nr = (number * -1 % 10) * -1
+    if nr > 5:
+        print(f"Last digit of {number} is {nr} and is greater than 5")
+    elif nr == 0:
+        print(f"Last digit of {number} is {nr} and is 0")
+    elif nr < 6 and nr != 0:
+        print(f"Last digit of {number} is {nr} and is less than 6 and not 0")
 else:
-    l = number % 10
-    if l > 5:
-        print(f"Last digit of {number} is {l} and is greater than 5")
-    elif l == 0:
-        print(f"Last digit of {number} is {l} and is 0")
-    elif l < 6 and l != 0:
-        print(f"Last digit of {number} is {l} and is less than 6 and not 0")
+    nr = number % 10
+    if nr > 5:
+        print(f"Last digit of {number} is {nr} and is greater than 5")
+    elif nr == 0:
+        print(f"Last digit of {number} is {nr} and is 0")
+    elif nr < 6 and nr != 0:
+        print(f"Last digit of {number} is {nr} and is less than 6 and not 0")
