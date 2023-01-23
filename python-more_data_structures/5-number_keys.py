c4f49fa94a125ccc9866506695f4a19632e143b1
Katrolli <ennio_cano@live.com>
2023-01-23 15:18:44 -0800

    nr of keys

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    for i,smth in enumerate(a_dictionary):
+    total = 0
+    for i, smth in enumerate(a_dictionary):
         total = i
+    if total == 0:
+        return 0
     return total + 1
