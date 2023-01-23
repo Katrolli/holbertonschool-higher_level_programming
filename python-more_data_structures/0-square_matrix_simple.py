16d374b39fc15a719e59292b50b96625f2e1c017
Katrolli <ennio_cano@live.com>
2023-01-23 10:01:38 -0800

    matrix adding

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for _list in matrix:
-        tmp =[]
+        tmp = []
         for nr in _list:
             tmp.append(nr * nr)
         new_matrix.append(tmp)
