3d5241568aed4e222d2e3797af8fa78c605f1638
Katrolli <ennio_cano@live.com>
2023-01-23 05:29:19 -0800

    finding max int

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is not None:
-        max = my_list[0]
-        for x in my_list:
-            if x > max:
-                max = x
-        return (max)
+    if len(my_list) == 0:
+        return None
+    max = my_list[0]
+    for x in my_list:
+        if x > max:
+             max = x
+    return (max)
