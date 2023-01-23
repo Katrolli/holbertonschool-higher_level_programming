883d9290dfecd35c2af2cda7c9d9fc46784f0ff1
Katrolli <ennio_cano@live.com>
2023-01-23 15:28:52 -0800

    nr of kyes

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     total = 0
+    if len(a_dictionary) == 0:
+        return 0
     for i, smth in enumerate(a_dictionary):
         total = i
-    if total == 0:
-        return 0
-    return total + 1
+    if total != 0:
+        return total + 1
+    else:
+        return 1
