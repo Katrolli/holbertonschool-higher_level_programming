8f950fc72a775823d559f04a79db2cb03a5fba29
Katrolli <ennio_cano@live.com>
2023-01-24 03:25:47 -0800

    best values

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    max = 0
+    values = list(a_dictionary.values())
+    for i, nr in enumerate(values):
+        if nr > max:
+            max = nr
+    key_list = list(a_dictionary.keys())
+    position = values.index(max)
+    return key_list[position]
