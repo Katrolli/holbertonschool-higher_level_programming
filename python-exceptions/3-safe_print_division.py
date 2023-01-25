31bf4ffde0d51398444d04ee6031d8f16eb48bf5
Katrolli <ennio_cano@live.com>
2023-01-25 06:01:34 -0800

    divion and case handling

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+def safe_print_division(a, b):
+    c = 0
+    try:
+        c = a / b
+    except ZeroDivisionError:
+        c = None
+    finally:
+        print("Inside result: {}".format(c))
+        return c
