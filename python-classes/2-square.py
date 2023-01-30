758acb80ba52e74d5bc53e64db0db2fd392bdf8e
Katrolli <ennio_cano@live.com>
2023-01-30 06:47:03 -0800

    raising exceptions if size is not int or smaller than 0

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""square module with size"""
+
+
+class Square:
+    '''init function with size 0'''
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self._size = size
