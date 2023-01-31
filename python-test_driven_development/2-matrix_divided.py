10c35965ebf4a5d5556b018521f96efc42fdf8bd
Katrolli <ennio_cano@live.com>
2023-01-31 04:35:02 -0800

    matrix test_driven

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' function that divides elements of matrix by div'''
+
+
+def matrix_divided(matrix, div):
+    ''' checking possibile errors and dividing'''
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = []
+    row_len = len(matrix[0])
+    for i, row in enumerate(matrix):
+        tmp = []
+        if len(row) != row_len:
+            raise TypeError("Each row of the matrix must have the same size")
+        for nr in row:
+            if type(nr) not in [int, float]:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            nr = int(nr)
+            tmp.append(round(nr / div, 2))
+        new_matrix.append(tmp)
+    return new_matrix
