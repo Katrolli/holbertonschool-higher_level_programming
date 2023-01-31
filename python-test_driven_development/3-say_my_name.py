a47b50c01e42cce5e34c5cbf660e3cd9ff46732a
Katrolli <ennio_cano@live.com>
2023-01-31 06:42:41 -0800

    printing your name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,7 +2,9 @@
 
 '''function that prints first and last name as string'''
 
+
 def say_my_name(first_name, last_name=""):
+    '''this is a documented function'''
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
