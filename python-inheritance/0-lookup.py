85d2fcd30182a272ea882e38ca06abe1318e5c79
Katrolli <ennio_cano@live.com>
2023-02-06 09:36:44 -0800

    dir()

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Module that prints available attributes and methods of obj'''
+
+
+def lookup(obj):
+    '''function that returns attributes and methods'''
+    return dir(obj)
