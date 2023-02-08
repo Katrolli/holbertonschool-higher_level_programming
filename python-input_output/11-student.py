d861666ee08a324fa298318adfe9344e4d7e4e57
Katrolli <ennio_cano@live.com>
2023-02-08 05:31:20 -0800

    last fix

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+''' Module of Student class'''
+
+
+class Student:
+    '''Student class'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        d = {}
+        if attrs is None:
+            for key, value in self.__dict__.items():
+                d[key] = self.__dict__[key]
+            return d
+        else:
+            for key, value in self.__dict__.items():
+                # if all(isinstance(item, str) for item in attrs):
+                if key in attrs:
+                    d[key] = self.__dict__[key]
+        return d
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            if key in self.__dict__.keys():
+                self.__dict__[key] = value
