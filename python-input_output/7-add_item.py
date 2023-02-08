e79be9fd80e660b0f8f2e6e984a27690c62eafc5
Katrolli <ennio_cano@live.com>
2023-02-07 18:10:01 -0800

    fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,6 +11,7 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 file_path = os.path.join(os.getcwd(), "add_item.json")
 filename = 'add_item.json'
 my_list = []
+tmp_list = []
 
 for i in range(len(sys.argv)):
     if i > 0:
@@ -24,9 +25,9 @@ if not os.path.isfile(filename):
 with open('add_item.json', 'r', encoding="utf-8") as a_file:
     content = json.load(a_file)
     tmp_list = list(content)
-    for item in tmp_list:
-        if item not in my_list:
-            my_list.append(item)
+    for item in my_list:
+        if item not in tmp_list:
+            tmp_list.append(item)
 
-save_to_json_file(my_list, filename)
+save_to_json_file(tmp_list, filename)
 new_list = load_from_json_file(filename)
