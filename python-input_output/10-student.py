ee3dcb44ec99ef372ef01358aa31df46ec90c554
Katrolli <ennio_cano@live.com>
2023-02-08 05:12:24 -0800

    student class get attribute

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' Module of Student class'''
+
+
+class Student:
+    '''Student class'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        d = {}
+        if attrs is None:
+            for key, value in self.__dict__.items():
+                d[key] = self.__dict__[key]
+            return d
+        else:
+            for key, value in self.__dict__.items():
+                # if all(isinstance(item, str) for item in attrs):
+                if key in attrs:
+                    d[key] = self.__dict__[key]
+        return d
