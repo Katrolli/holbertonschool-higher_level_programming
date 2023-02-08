d3636440a6dd8dfe4f59f32cb29a27fc9ee7f0ba
Katrolli <ennio_cano@live.com>
2023-02-07 17:49:00 -0800

    json read and write

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,16 +12,20 @@ file_path = os.path.join(os.getcwd(), "add_item.json")
 filename = 'add_item.json'
 my_list = []
 
-if not os.path.isfile(filename):
-    with open(filename, 'w', encoding="utf-8") as a_file:
-        a_file.write("")
 for i in range(len(sys.argv)):
     if i > 0:
         my_list.append(sys.argv[i])
         i += 1
     i += 1
-with open(filename, 'w', encoding="utf-8") as a_file:
-    a_file.write("")
+if not os.path.isfile(filename):
+    with open(filename, 'w', encoding="utf-8") as a_file:
+        save_to_json_file(my_list, file_path)
 
-save_to_json_file(my_list, file_path)
+with open('add_item.json', 'r', encoding="utf-8") as a_file:
+    content = json.load(a_file)
+    tmp_list = list(content)
+    for item in tmp_list:
+        if item not in my_list:
+            my_list.append(item)
+
 new_list = load_from_json_file(file_path)
