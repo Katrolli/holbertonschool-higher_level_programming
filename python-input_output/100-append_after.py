07d1b7ff5eb92b06b8cb69cc48de48b290457b62
Katrolli <ennio_cano@live.com>
2023-02-24 11:27:55 -0800

    search file and add line

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def append_after(filename="", search_string="", new_string=""):
+    '''Search file and add line'''
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    for i, line in enumerate(lines):
+        if search_string in line:
+            lines.insert(i + 1, new_string)
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.writelines(line)
