35a0f2eb48562567273baf1e2e7ed2ae7fccdaa7
Katrolli <ennio_cano@live.com>
2023-02-24 16:12:59 -0800

    fixed

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-'''append func'''
+'''Function that parses file line per line,
+adds new_string is search_string is found'''
 
 
 def append_after(filename="", search_string="", new_string=""):
     '''Search file and add line'''
     with open(filename, 'r') as file:
         lines = file.readlines()
-    for i, line in enumerate(lines):
-        if search_string in line:
+    i = 0
+    while i < len(lines):
+        if search_string in lines[i]:
             lines.insert(i + 1, new_string)
+            i += 1
+        i += 1
     with open(filename, 'w') as file:
         for line in lines:
             file.writelines(line)
