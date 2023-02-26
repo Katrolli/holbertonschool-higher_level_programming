1985f263f343f44b8cebe922b15f34c4cbe0ade1
Katrolli <ennio_cano@live.com>
2023-02-26 06:30:01 -0800

    adding smth

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 "Script that computes data"
 import sys
-
+import signal
+import traceback
 
 tot = 0
 count = 0
@@ -16,20 +17,38 @@ code_d = {
     "405": 0,
     "500": 0
 }
-for line in sys.stdin:
-    ls = line.split()
-    size = int(ls[len(ls) - 1])
-    s_code = ls[len(ls) - 2]
-    tot += size
-    code_list.append(s_code)
-    code_list.sort()
-    if count == 10 or count == 0:
+i = 0
+j = 0
+
+
+try:
+    while True:
+        i = 0
+        for line in sys.stdin:
+            ls = line.split()
+            size = int(ls[len(ls) - 1])
+            s_code = ls[len(ls) - 2]
+            tot += size
+            code_list.append(s_code)
+            code_list.sort()
+            if s_code in code_d:
+                code_d[s_code] += 1
+            i += 1
+            if i == 10:
+                break
         print("Filesize: {}".format(tot))
-        count += 1
-    elif count in range(1, 10):
-        if s_code in code_d:
-            code_d[s_code] += 1
-            print("{}: {}".format(code_list[count], code_d[s_code]))
-            count += 1
-            if count >= 10:
-                count = 0
+        for key, value in code_d.items():
+            if value != 0:
+                print(f"{key}: {value}")
+except KeyboardInterrupt:
+    for line in sys.stdin:
+        ls = line.split()
+        size = int(ls[len(ls) - 1])
+        s_code = ls[len(ls) - 2]
+        tot += size
+        code_list.append(s_code)
+        code_list.sort()
+    print("Filesize: {}".format(tot))
+    for key, value in code_d.items():
+        if value != 0:
+            print(f"{key}: {value}")
