370abc823ac42cac3f017924470d177a840748ba
Katrolli <ennio_cano@live.com>
2023-03-07 15:49:55 -0800

    showing priv of users

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- List all Privileges of user
-SELECT * FROM mysql.user WHERE user = 'user_0d_1' OR user = 'user_0d_2';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
