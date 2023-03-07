b4abb38959939f609b742aab48e1e31c7b0299d5
Katrolli <ennio_cano@live.com>
2023-03-07 08:19:46 -0800

    showing scores grouped by same score

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- showing score as numbers
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score; 
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC; 
