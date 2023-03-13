ad3578c6272f1489346085da748fd99951c83a4f
Katrolli <ennio_cano@live.com>
2023-03-13 06:26:39 -0700

    NOT ORM

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Script that lists all states'''
+import sys
+import MySQLdb
+
+
+u = sys.argv[1]
+p = sys.argv[2]
+d = sys.argv[3]
+
+
+db = MySQLdb.connect(host="localhost", port=3306, user=u, password=p, database=d)
+cr = db.cursor()
+cr.execute("SELECT * from states")
+states = cr.fetchall()
+for state in states:
+    print(state)
+cr.close()
+db.close()
