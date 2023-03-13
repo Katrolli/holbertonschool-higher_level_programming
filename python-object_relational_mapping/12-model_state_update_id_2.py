c76ba7d5aa15ad71a6b51cf7eb5e576a197e1fe8
Katrolli <ennio_cano@live.com>
2023-03-13 13:39:54 -0700

    changing state when id is 2

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Module that list states in orm'''
+from model_state import Base, State
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                            sys.argv[1],
+                            sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    update_state = session.query(State).filter(State.id == 2).one()
+
+    update_state.name = 'New Mexico'
+    session.commit()
+    session.close()
