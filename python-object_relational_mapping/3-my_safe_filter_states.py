c9d5c1c29a3ddd39803d1be3ccbebc6ba303498c
Katrolli <ennio_cano@live.com>
2023-03-13 07:32:39 -0700

    protected against injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Module that lists state searched by argv[4]'''
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], password=sys.argv[2],
+                         db=sys.argv[3], charset='utf8')
+    cr = db.cursor()
+    cr.execute("SELECT * from states WHERE states.name LIKE %s",
+               (sys.argv[4], ))
+    states = cr.fetchall()
+    for state in states:
+        print(state)
+    cr.close()
+    db.close()
