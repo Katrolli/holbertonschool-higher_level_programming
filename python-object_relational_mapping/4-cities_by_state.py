1981e836049ad52cb65eec28fc5de78662bebc54
Katrolli <ennio_cano@live.com>
2023-03-13 07:50:50 -0700

    listing city and state

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Module that lists state searched by argv[4]'''
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], password=sys.argv[2],
+                         db=sys.argv[3], charset='utf8')
+    cr = db.cursor()
+    cr.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON states.id=cities.id")
+    cities = cr.fetchall()
+    for city in cities:
+        print(city)
+    cr.close()
+    db.close()
