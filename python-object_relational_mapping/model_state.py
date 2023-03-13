49d3f5de2559fd2267c263974cffb21a46ad90b6
Katrolli <ennio_cano@live.com>
2023-03-13 10:14:48 -0700

    testing class state

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usrbin/python3
+'''Module that creates state class'''
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
