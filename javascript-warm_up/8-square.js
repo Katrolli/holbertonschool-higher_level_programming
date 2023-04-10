979878fa3bb81b58036e5d83dacdbeee74d9dbe4
Katrolli <ennio_cano@live.com>
2023-04-10 17:39:48 +0200

    printing square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const n = Number(process.argv[2]);
+
+if (isNaN(n)) {
+  console.log('Missing size');
+}
+
+for (let i = 0; i < n; i++) {
+  let row = '';
+  for (let j = 0; j < n; j++) {
+    row += 'x';
+  }
+  console.log(row);
+}
