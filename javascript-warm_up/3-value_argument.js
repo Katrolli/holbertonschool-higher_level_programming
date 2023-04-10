1031e60b4d83af74be91d0f720b2eeb0eb87ec46
Katrolli <ennio_cano@live.com>
2023-04-10 16:40:11 +0200

    argv count

---
+++ javascript-warm_up/3-value_argument.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+import { argv } from 'node:process';
+
+let count = 0;
+
+for (let i = 0; argv[i] != null; i++) {
+  count++;
+}
+
+let i = 0;
+while (i < count) {
+  i += 2;
+  if (i + 1 > count) {
+    console.log('No argument');
+    break;
+  } else {
+    console.log(argv[i]);
+    i += 1;
+  }
+}
