6c9731c59ca996d9c16e7fa32ebd01dddc1e6851
Katrolli <ennio_cano@live.com>
2023-04-10 18:21:52 +0200

    finding second biggest

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+import { argv } from 'node:process';
+
+if (argv.length === 2 || argv.length === 3) {
+  console.log(0);
+}
+function find_big(array) {
+  let nr = 0;
+  let i = 2;
+  while (array[i] != null) {
+    if (array[i] < array[i + 1]) {
+      nr = array[i + 1];
+      i++;
+    }
+    i++;
+  }
+  return nr;
+}
+
+for (let i = 2; argv[i] != null; i++) {
+  if (argv[i] == find_big(argv) - 1) {
+    console.log(argv[i]);
+    break;
+  }
+}
