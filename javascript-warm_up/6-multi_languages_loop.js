7a81aa1485653173a3592c3f0c1b4886483e32c3
Katrolli <ennio_cano@live.com>
2023-04-10 17:23:42 +0200

    printing array items

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arr = ['C is fun', 'Pythin is cool', 'JavaScript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; arr[i] != null; i++) {
   console.log(arr[i]);
