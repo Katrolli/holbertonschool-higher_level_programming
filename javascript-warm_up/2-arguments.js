00493fd6d86f3bc54679e52bf6343b489218db3b
Katrolli <ennio_cano@live.com>
2023-04-10 15:34:52 +0200

    count argv

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@ import { argv } from 'node:process';
 
 if (argv.length < 3) {
   console.log('No argument');
-} else {
+} else if (argv.length === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
