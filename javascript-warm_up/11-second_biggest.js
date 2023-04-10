952915437f86b8743f5c2566406f6ef95f9c4cd3
Katrolli <ennio_cano@live.com>
2023-04-10 20:27:05 +0200

    finding second biggest nr

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,10 +3,10 @@ import { argv } from 'node:process';
 
 if (argv.length === 2 || argv.length === 3) {
   console.log(0);
+} else {
+  let nums = argv.slice(2);
+  const max = Math.max(...nums);
+  nums = nums.filter((nr) => nr < max);
+  const secondMax = Math.max(...nums);
+  console.log(secondMax);
 }
-
-let nums = argv.slice(2);
-const max = Math.max(...nums);
-nums = nums.filter((nr) => nr < max);
-const secondMax = Math.max(...nums);
-console.log(secondMax);
