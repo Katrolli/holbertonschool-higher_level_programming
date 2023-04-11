b163521084565b4182473d78aba8197c6293c133
Katrolli <ennio_cano@live.com>
2023-04-11 13:33:15 +0200

    conditional w and h and adding print func

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,7 +2,7 @@
 const Sq1 = require('./5-square');
 
 class Square extends Sq1 {
-  charPrint (c = 'x') {
+  charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       let row = '';
       for (let j = 0; j < this.width; j++) {
