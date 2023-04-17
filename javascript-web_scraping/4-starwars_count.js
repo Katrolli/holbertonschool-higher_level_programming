ca9b5d8710c10d9fe806dd1249c3b1e31bf64c8d
Katrolli <ennio_cano@live.com>
2023-04-17 21:56:04 +0200

    scraping lorem impsum

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,27 +4,6 @@ const request = require('request');
 
 const apiCall = process.argv[2];
 
-// function getMovies(call) {
-//   request.get(call, function (error, response, body) {
-//     if (error) {
-//       console.log('error', error);
-//     } else {
-//       const search = JSON.parse(body).results[0].characters;
-//       const char = String(search[15]);
-//       request.get(char, function (error, response, body) {
-//         if (error) {
-//           console.log('error', error);
-//         } else {
-//           const films = JSON.parse(body).films;
-//           console.log(`${films.length}`);
-//         }
-//       });
-//     }
-//   });
-// }
-
-// getMovies(apiCall);
-
 request.get(apiCall, function (error, response, body) {
   if (error) {
     console.log('error', err);
---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const apiCall = process.argv[2];
+const fileToWrite = process.argv[3];
+
+request.get(apiCall, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(fileToWrite, body, 'utf-8', (error) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+    });
+  }
+});
