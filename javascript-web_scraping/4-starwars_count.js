569c0207c1316baf7a7428792407f30d2b61f976
Katrolli <ennio_cano@live.com>
2023-04-17 21:28:28 +0200

    testing

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,31 +4,40 @@ const request = require('request');
 
 const apiCall = process.argv[2];
 
-function getMovies(call) {
-  request.get(call, function (error, response, body) {
-    if (error) {
-      console.log('error', error);
-    } else {
-      const search = JSON.parse(body).results[0].characters;
-      const char = String(search[15]);
-      request.get(char, function (error, response, body) {
-        if (error) {
-          console.log('error', error);
-        } else {
-          const films = JSON.parse(body).films;
-          console.log(`${films.length}`);
+// function getMovies(call) {
+//   request.get(call, function (error, response, body) {
+//     if (error) {
+//       console.log('error', error);
+//     } else {
+//       const search = JSON.parse(body).results[0].characters;
+//       const char = String(search[15]);
+//       request.get(char, function (error, response, body) {
+//         if (error) {
+//           console.log('error', error);
+//         } else {
+//           const films = JSON.parse(body).films;
+//           console.log(`${films.length}`);
+//         }
+//       });
+//     }
+//   });
+// }
+
+// getMovies(apiCall);
+
+request.get(apiCall, function (error, response, body) {
+  if (error) {
+    console.log('error', err);
+  } else {
+    const data = JSON.parse(body).results;
+    let count = 0;
+    for (const info of data) {
+      for (const chars in info.characters) {
+        if (chars.includes('18')) {
+          count++;
         }
-      });
+      }
     }
-  });
-}
-
-getMovies(apiCall);
-
-// request.get(call, function (error, response, body) {
-//   if (error) {
-//     console.log('error', err);
-//   } else {
-//     const movies = JSON.parse(body).results;
-//   }
-// });
+    console.log(count);
+  }
+});
